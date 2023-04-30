d4bb43f5ac9bea7f25c74e9ef530a72650e4aa75
sudarsana kasireddy <sudaredd@gmail.com>
2023-04-29 21:47:56 -0400

    use fake api, extract payload from message

--- consumer.py
+++ consumer.py
@@ -1,10 +1,15 @@
-import json
 from kafka import KafkaConsumer
 import logging
+
 topic_name = "test"
+import json
 
 consumer = KafkaConsumer(topic_name, bootstrap_servers=['localhost:9092'],
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')), group_id='demo',
                          auto_offset_reset='earliest')
 for message in consumer:
-    print(message)
+    msg = message.value
+    msg_dict = json.loads(msg)
+    print(msg)
+    print("userId {}".format(msg_dict['user_id']))
+    print("user name {}".format(msg_dict['user_name']))
